79388e6ab492ca87974a39fe9899ded3aaf56438
OmerAslani <aslaniomer21@gmail.com>
2025-02-18 20:29:10 +0100

    make Initial project

---
+++ templates/inc/menuIncView.php
@@ -0,0 +1,6 @@
+<nav class="nav-bar">
+    <a href="/">Home</a>
+    <a href="/public/?p=aboutMe">About Me</a>
+    <a href="/public/?p=history">History</a>
+    <a href="/public/?p=contact">Contact</a>
+</nav>
